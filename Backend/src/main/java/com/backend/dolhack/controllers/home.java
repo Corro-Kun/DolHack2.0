eef54465e6646b9e122ef6db270b6612885f85df
Corro-Kun <networpava@gmail.com>
2023-09-04 18:51:46 -0500

    Repositorie User

---
+++ Backend/src/main/java/com/backend/dolhack/controllers/home.java
@@ -0,0 +1,42 @@
+package com.backend.dolhack.controllers;
+
+import com.backend.dolhack.models.message;
+import com.backend.dolhack.repositories.HomeRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/home")
+@RestController
+public class home {
+
+    private final HomeRepositorio repositorio;
+
+    @Autowired
+    public home(HomeRepositorio repositorio){
+        this.repositorio = repositorio;
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+    @GetMapping("/teacher")
+    public ResponseEntity profesor(){
+        try {
+            return ResponseEntity.ok().body(repositorio.getUserList("profesor"));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new message(e.getMessage()));
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+    @GetMapping("/student")
+    public ResponseEntity estudiante(){
+        try {
+            return ResponseEntity.ok().body(repositorio.getUserList("estudiante"));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new message(e.getMessage()));
+        }
+    }
+}
---
+++ Backend/src/main/java/com/backend/dolhack/repositories/HomeRepositorio.java
@@ -0,0 +1,22 @@
+package com.backend.dolhack.repositories;
+
+import com.backend.dolhack.models.home.userListModel;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomeRepositorio {
+    private final JdbcTemplate sql;
+ 
+    @Autowired
+    public HomeRepositorio(JdbcTemplate sql){
+        this.sql = sql;
+    }
+ 
+    public List<userListModel> getUserList(String rol){
+        String querry = "SELECT usuario.idusuario,usuario.nombre, usuario.apellido, usuario.biografia, usuario.foto, usuario.banner, correo.correo, rol.rol FROM usuario JOIN correo ON correo.usuario_idusuario = usuario.idusuario JOIN rol ON rol.idrol = usuario.rol_idrol WHERE rol.rol = '"+ rol +"' LIMIT 9";
+        return sql.query(querry, (rs, rowNum) -> new userListModel(rs.getString("nombre"), rs.getString("apellido"), rs.getString("biografia"), rs.getString("foto"), rs.getString("banner"), rs.getString("rol"), rs.getString("correo"), rs.getString("idusuario")));
+    }
+}
