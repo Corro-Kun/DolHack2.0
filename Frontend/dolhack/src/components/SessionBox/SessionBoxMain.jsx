9203966160ea94348c36aa202353abab9f1c8a28
Corro-Kun <networpava@gmail.com>
2024-02-05 18:15:18 -0500

    fixed error button the frist home

--- Frontend/dolhack/src/components/SessionBox/SessionBoxMain.jsx
+++ Frontend/dolhack/src/components/SessionBox/SessionBoxMain.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 import "./SessionBoxMain.css";
 
-function SessionBoxMain({icon,title,descripcion,button}){
+function SessionBoxMain({icon,title,descripcion,button,path}){
+  const navigate = useNavigate();
     return(
         <div className="SessionBoxMain">
             <h1>{icon}</h1>
             <h2>{title}</h2>
             <p>{descripcion}</p>
-            <button>{button}</button>
+            <button onClick={()=> navigate(path)}>{button}</button>
         </div>
     );
 }
--- Frontend/dolhack/src/page/main/FristHome.jsx
+++ Frontend/dolhack/src/page/main/FristHome.jsx
@@ -11,9 +11,9 @@ function FristHome(){
             <BarMain />
             <IntroMain />
             <Session title="¿Eres nuevo?">
-                <SessionBoxMain  icon="👋" title="¡Hola!" descripcion="Si eres nuevo en la plataforma, te recomendamos que te registres para que puedas disfrutar de todos los beneficios que te ofrece la plataforma." button="Registrarse" />
-                <SessionBoxMain  icon="ℹ️" title="Más Información" descripcion="Ve a información Allí encontrarás una gran cantidad de detalles y datos interesantes que seguramente te serán de utilidad. ¡No dudes en explorar y aprender más!" button="Información" />
-                <SessionBoxMain icon="🛠️" title="Servicios" descripcion="Te animamos a explorar nuestra sección de servicios, Aquí encontrarás una variedad de servicios diseñados para satisfacer tus necesidades. " button="servicios" />
+                <SessionBoxMain  icon="👋" title="¡Hola!" descripcion="Si eres nuevo en la plataforma, te recomendamos que te registres para que puedas disfrutar de todos los beneficios que te ofrece la plataforma." button="Registrarse" path="/login" />
+                <SessionBoxMain  icon="ℹ️" title="Más Información" descripcion="Ve a información Allí encontrarás una gran cantidad de detalles y datos interesantes que seguramente te serán de utilidad. ¡No dudes en explorar y aprender más!" button="Información" path="/information" />
+                <SessionBoxMain icon="🛠️" title="Servicios" descripcion="Te animamos a explorar nuestra sección de servicios, Aquí encontrarás una variedad de servicios diseñados para satisfacer tus necesidades. " button="servicios" path="/services" />
             </Session>
         </Body>
     );
